a01c072c76b491a5bafc8ad7fc91a99b62233424
caroline tornquist <caroline.22@dartmouth.edu>
2020-08-14 11:52:44 -0400

    started auth

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,50 @@
+import jwt from 'jwt-simple';
+import dotenv from 'dotenv';
+import User from '../models/user_model';
+
+dotenv.config({ silent: true });
+
+// encodes a new token for a user object
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.AUTH_SECRET);
+}
+
+export const signin = (req, res, next) => {
+  res.send({ token: tokenForUser(req.user) });
+};
+
+// eslint-disable-next-line consistent-return
+export const signup = (req, res, next) => {
+  const { email } = req.body;
+  const { password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send('You must provide email and password');
+  }
+
+  // 🚀 TODO:
+  // here you should do a mongo query to find if a user already exists with this email.
+  // if user exists then return an error. If not, use the User model to create a new user.
+  // Save the new User object
+  // this is similar to how you created a Post
+  // and then return a token same as you did in in signin
+  User.findOne({ email })
+    .then((result) => {
+      if (result) {
+        res.json({ message: 'email used is already taken' });
+      } else {
+        const user = new User({
+          email,
+          password,
+        });
+        user.save()
+          .then(() => {
+            res.send({ token: tokenForUser(user) });
+          })
+          .catch((error) => {
+            res.status(500).json({ error: 'error saving user' });
+          });
+      }
+    });
+};
---
+++ src/models/user_model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from 'mongoose';
+import bcryptjs from 'bcryptjs';
+
+// create a User with email/pw
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+}, {
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+  timestamps: true,
+});
+
+UserSchema.pre('save', function beforeUserSave(next) {
+  // this is a reference to our model
+  // the function runs in some other context so DO NOT bind it
+  const user = this;
+
+  // only salt/hash if has been modified
+  if (!user.isModified('password')) return next();
+
+  // when done run the **next** callback with no arguments
+  // call next with an error if you encounter one
+  // return next();
+  bcryptjs.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcryptjs.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      // Store hash in your password DB. Set the user.password to the hash and return next()
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, callback) {
+  // return callback(null, comparisonResult) for success
+  // or callback(error) in the error case
+  bcryptjs.compare(candidatePassword, this.password, (err, res) => {
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(null, res);
+    }
+  });
+};
+
+// create UserModel class from schema
+const User = mongoose.model('Poll', UserSchema);
+
+export default User;
